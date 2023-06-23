8e260dd09ce2aa38e09bff847bb8d9d54c0032b0
MarkRybaDigital <mark@rybadigital.com>
2023-06-22 21:31:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    console.log(req.params.page)
+    console.log(req.query["page"])
     res.send("Yo")
 })
 app.listen(process.env.PORT || 3000)
