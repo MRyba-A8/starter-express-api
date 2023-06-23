1f9b184eb93e87b61bf99901a9da9947b0117661
MarkRybaDigital <mark@rybadigital.com>
2023-06-22 23:01:40 -0400

    Adding Axios and company code

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
+
+const headers = {
+    "content-type": "application/json",
+    "authorization": `Bearer ${process.env.HS_KEY}`
+}
+
+async function getCompanies(companies = [], after = 0) {
+    const searchURL = "https://api.hubapi.com/crm/v3/objects/companies/search";
+    const searchBody = {
+        "filterGroups": [
+            {
+                "filters": [
+                    {
+                        "propertyName": "packback_campus_id",
+                        "operator": "HAS_PROPERTY"
+                    }
+                ]
+            }
+        ],
+        "properties": [
+            "name"
+        ],
+        "limit": 100,
+        "after": after
+    }
+    const companyBatch = await axios.post(searchURL, searchBody, { headers: headers });
+    return companyBatch;
+}
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     console.log(req.query["page"])
-    res.send("Yo")
+    res.send(getCompanies());
 })
 app.listen(process.env.PORT || 3000)
