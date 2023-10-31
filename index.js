7b6ae0738e817aab4f75ce8cf4a3f5e5acc73d09
MarkRybaDigital <Mark@aptitude8.com>
2023-10-30 23:11:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const headers = {
     "authorization": `Bearer ${process.env.HS_KEY}`
 }
 
+const cdsHeaders = {
+    "content-type": "application/json",
+    "authorization": `Bearer ${process.env.DCS_HS_KEY}`
+}
+
 const limiter = new Bottleneck({
     minTime: 333
 });
@@ -56,6 +61,43 @@ async function getCompanies(companies = [], after = 0) {
     }
 }
 
+async function getAllEvents(after = null) {
+    try {
+        const baseUrl = 'https://api.hubapi.com/crm/v3/objects/events';
+        const properties = [
+            'event_type',
+            'city',
+            'date___time',
+            'start_time',
+            'end_time',
+            'event_name',
+            'description',
+            'event_registration_link',
+            'event_host',
+            'event_cost_options',
+            'location_address',
+        ];
+
+        // Construct the URL with query parameters
+        const queryParams = {
+            properties: properties.join(','),
+            limit: 100, // Adjust the limit as needed
+            after: after, // Use this for pagination
+        };
+
+        const response = await axios.get(baseUrl, {
+            params: queryParams,
+            headers: {
+                "content-type": "application/json",
+                "authorization": `Bearer ${process.env.DCS_HS_KEY}`
+            },
+        });
+        return response.data;
+    } catch (error) {
+        throw new Error(`Error fetching events: ${error.message}`);
+    }
+}
+
 app.use(cors())
 
 app.get('/companies.json', async (req, res) => {
@@ -89,5 +131,16 @@ app.get('/getallcompanies', async (req, res) => {
         console.log(error);
     }
 })
+
+app.get('/events', async (req, res) => {
+    try {
+        const after = req.query.after || null; // Use the "after" query parameter if present, otherwise, set it to null
+        const events = await getAllEvents(after); // Pass it to the function
+        res.json(events);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 app.listen(process.env.PORT || 3000)
